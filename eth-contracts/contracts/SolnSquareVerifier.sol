1aae28c2eb501c7abbbac29d2748600f90ff2128
Marc Gardeya <marc@gardeya.de>
2019-05-18 17:51:55 +0200

    added ZoKrates verfier and tests

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,49 +1,62 @@
 pragma solidity >=0.4.21 <0.6.0;
 
+import "./ERC721Mintable.sol";
+
 // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
 
-
+/*
+contract Verifier {
+    function verifyTx(
+        uint[2] memory a,
+        uint[2] memory a_p,
+        uint[2][2] memory b,
+        uint[2] memory b_p,
+        uint[2] memory c,
+        uint[2] memory c_p,
+        uint[2] memory h,
+        uint[2] memory k,
+        uint[2] memory input
+    ) public returns (bool r);
+}
+*/
 
 // TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
 
+contract SolnSquareVerifier is CustomERC721Token {}
 
+/*
+    Verifier verifierContract;
 
-// TODO define a solutions struct that can hold an index & an address
+    constructor(address verifierAddress, string memory name, string memory symbol) UdacityRealStateTitle(name, symbol) public {
+        verifierContract = Verifier(verifierAddress);
+    }
 
+    // TODO define a solutions struct that can hold an index & an address
 
-// TODO define an array of the above struct
+    struct {
+        uint256 index;
+        address a;
+    } Solution;
 
+    // TODO define an array of the above struct
+    Solution[] solutions;
 
-// TODO define a mapping to store unique solutions submitted
+    // TODO define a mapping to store unique solutions submitted
+    mapping(?? => Solution) uniqueSolutions;
 
-
-
-// TODO Create an event to emit when a solution is added
-
-
-
-// TODO Create a function to add the solutions to the array and emit the event
-
-
-
-// TODO Create a function to mint new NFT only after the solution has been verified
-//  - make sure the solution is unique (has not been used before)
-//  - make sure you handle metadata as well as tokenSuplly
-
-
-
+    // TODO Create an event to emit when a solution is added
 
 
 
+    // TODO Create a function to add the solutions to the array and emit the event
 
 
 
+    // TODO Create a function to mint new NFT only after the solution has been verified
+    //  - make sure the solution is unique (has not been used before)
+    //  - make sure you handle metadata as well as tokenSuplly
 
-
-
-
-
-
+}
 
 
 
@@ -54,3 +67,18 @@ pragma solidity >=0.4.21 <0.6.0;
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,10 +1,10 @@
 // migrating the appropriate contracts
 var CustomERC721Token = artifacts.require("CustomERC721Token");
-//var SquareVerifier = artifacts.require("./SquareVerifier.sol");
+var SquareVerifier = artifacts.require("Verifier.sol");
 //var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(CustomERC721Token);
-  //deployer.deploy(SquareVerifier);
+  deployer.deploy(SquareVerifier);
   //deployer.deploy(SolnSquareVerifier);
 };
